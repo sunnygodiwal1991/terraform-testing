09de079f285dd53f1dea33c381ef30b95e93dc9c
sunnygodiwal1991 <60594463+sunnygodiwal1991@users.noreply.github.com>
2025-02-11 14:03:11 +0530

    first

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "my-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "my-vpc"
+    Env  = "qa"
+  }
+}
